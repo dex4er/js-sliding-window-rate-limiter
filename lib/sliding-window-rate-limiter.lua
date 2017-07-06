33f81e4ae7f89f5eb6ce5a0af0f30b6596e95a59
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2017-07-06 16:30:10 +0200

    Renamed period to interval variable

--- lib/sliding-window-rate-limiter.lua
+++ lib/sliding-window-rate-limiter.lua
@@ -7,10 +7,10 @@
 
 local key     = KEYS[1]
 
-local period  = tonumber(ARGV[1])
-local limit   = tonumber(ARGV[2])
-local ttl     = tonumber(ARGV[3])
-local reserve = (ARGV[4] ~= '0')
+local interval = tonumber(ARGV[1]) -- seconds
+local limit    = tonumber(ARGV[2]) -- number
+local ttl      = tonumber(ARGV[3]) -- seconds
+local reserve  = (ARGV[4] ~= '0')  -- 0 or 1
 
 -- script effects replication is available since Redis 3.2
 redis.replicate_commands()
@@ -18,7 +18,7 @@ redis.replicate_commands()
 local redistime = redis.call("TIME")
 local ts = redistime[1] * 1e6 + redistime[2]
 
-local startwindow = ts - period * 1e6
+local startwindow = ts - interval * 1e6
 redis.call("ZREMRANGEBYSCORE", key, "-inf", startwindow)
 
 local usage = tonumber(redis.call("ZCOUNT", key, 1, ts))
