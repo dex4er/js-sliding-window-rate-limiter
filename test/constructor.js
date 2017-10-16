ac6c1bfc05b00ffb5a60fe51873c4fefbb09d7fc
Tadeusz Wawszczak <Tadeusz.Wawszczak@mapp.com>
2017-10-16 18:43:06 +0200

    extend constructor tests

--- test/constructor.js
+++ test/constructor.js
@@ -7,15 +7,22 @@ const chai = require('chai')
 chai.use(require('dirty-chai'))
 chai.should()
 
-for (const backend of ['Memory', 'Redis']) {
+const backendClasses = {
+  Memory: require('../lib/memory-sliding-window-rate-limiter'),
+  Redis: require('../lib/redis-sliding-window-rate-limiter'),
+  SafeRedis: require('../lib/safe-redis-sliding-window-rate-limiter')
+}
+
+for (const backend of ['Memory', 'Redis', 'SafeRedis']) {
   Feature(`Test sliding-window-rate-limiter module constructor with ${backend} backend`, () => {
     const SlidingWindowRateLimiter = require('../lib/sliding-window-rate-limiter')
 
     let limiter
     let redis
+    let safe = false
 
     Scenario('basic usage', () => {
-      if (backend === 'Redis') {
+      if (backend === 'Redis' || backend === 'SafeRedis') {
         const TEST_REDIS_URL = process.env.TEST_REDIS_URL
         const redisModule = TEST_REDIS_URL ? 'ioredis' : '../mock/mock-ioredis'
         const Redis = require(redisModule)
@@ -25,19 +32,29 @@ for (const backend of ['Memory', 'Redis']) {
         })
       }
 
+      if (backend === 'SafeRedis') {
+        Given('safe flag', () => {
+          safe = true
+        })
+      }
+
       When('create simple limiter', () => {
-        limiter = SlidingWindowRateLimiter.createLimiter({ redis })
+        limiter = SlidingWindowRateLimiter.createLimiter({ safe, redis })
       })
 
       Then('limiter exists', () => {
         limiter.should.have.property('reserve')
       })
 
+      And('limiter has correct class', () => {
+        limiter.should.be.instanceOf(backendClasses[backend])
+      })
+
       After('destroy limiter', () => {
         limiter.destroy()
       })
 
-      if (backend === 'Redis') {
+      if (backend === 'Redis' || backend === 'SafeRedis') {
         After('disconnect Redis', () => {
           redis.disconnect()
         })
