da13f7c06907a24bb8eba72678388cdf3532d6fd
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2019-08-06 10:21:08 +0200

    Use mocha-steps for tests

--- test/lib/steps.ts
+++ test/lib/steps.ts
@@ -6,17 +6,17 @@ export function Feature(what: string, how: () => void): Mocha.Suite {
 export function Scenario(what: string, how: () => void): Mocha.Suite {
   return describe("Scenario: " + what, how)
 }
-export function Given(what: string, how: (done: Mocha.Done) => void): Mocha.Test {
-  return it("Given " + what, how)
+export function Given(what: string, how: (this: Mocha.ISuiteCallbackContext) => void): Mocha.ISuite {
+  return step("Given " + what, how)
 }
-export function When(what: string, how: (done: Mocha.Done) => void): Mocha.Test {
-  return it("When " + what, how)
+export function When(what: string, how: (this: Mocha.ISuiteCallbackContext) => void): Mocha.ISuite {
+  return step("When " + what, how)
 }
-export function Then(what: string, how: (done: Mocha.Done) => void): Mocha.Test {
-  return it("Then " + what, how)
+export function Then(what: string, how: (this: Mocha.ISuiteCallbackContext) => void): Mocha.ISuite {
+  return step("Then " + what, how)
 }
-export function And(what: string, how: (done: Mocha.Done) => void): Mocha.Test {
-  return it("And " + what, how)
+export function And(what: string, how: (this: Mocha.ISuiteCallbackContext) => void): Mocha.ISuite {
+  return step("And " + what, how)
 }
 export function After(callback: (this: Mocha.Context, done: Mocha.Done) => any): void {
   after(callback)
